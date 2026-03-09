61de5273ae3162be3e828dc3526b5874c148a7c3
dana7677 <danote76772@gmail.com>
2026-03-09 18:34:55 +0100

    Subida funcionalidad básica

--- app/src/main/java/com/example/proyectorobotica/MainActivity.kt
+++ app/src/main/java/com/example/proyectorobotica/MainActivity.kt
@@ -5,8 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,29 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val boton = findViewById<Button>(R.id.botonEncender)
+
+        boton.setOnClickListener {
+            enviarComando("http://192.168.1.50/on")
+        }
+    }
+
+}
+fun enviarComando(urlString: String) {
+    thread {
+        try {
+            val url = URL(urlString)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+            connection.connectTimeout = 5000
+            connection.readTimeout = 5000
+
+            val responseCode = connection.responseCode
+            println("Respuesta: $responseCode")
+
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
